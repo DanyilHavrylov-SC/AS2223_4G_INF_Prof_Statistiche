3826570ad2cc2700ad0c87f07715c3d30f8b0026
Utente <Utente@INFO3CATTEDRA>
2022-11-07 11:08:47 +0100

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_Prof_Statistiche/Statistiche.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_Prof_Statistiche
+{
+    public class Statistiche
+    {
+        int n_Numeri = 0;
+        double somma = 0;
+        double sommaPesi = 0;
+
+        public Statistiche()
+        {
+
+        }
+
+        /// <summary>
+        /// Aggiungi un valore senza peso per le statistiche (media aritmetica)
+        /// </summary>
+        /// <param name="valore"></param>
+        public void AddValue(int valore)
+        {
+            n_Numeri++;
+            somma += valore;
+        }
+
+        /// <summary>
+        /// Aggiungi un valore con peso per le statistiche (media ponderata)
+        /// </summary>
+        /// <param name="valore"></param>
+        /// <param name="peso"></param>
+        public void AddValue(int valore, double peso)
+        {
+            n_Numeri++;
+            somma += valore * peso;
+            sommaPesi += peso;
+        }
+
+        /// <summary>
+        /// Ritorna la media aritmetica dei valori :
+        ///     mediaAritmetica = sommaValori/numeroValori
+        /// </summary>
+        /// <returns></returns>
+        public double MediaAritmetica()
+        {
+            // calcola la media aritmetica
+            return somma / n_Numeri;
+        }
+
+        /// <summary>
+        /// Ritorna la media ponderata dei valori :
+        ///     mediaPonderata = sommaValoriPerPeso/sommaPesi
+        /// </summary>
+        /// <returns></returns>
+        public double MediaPonderata()
+        {
+            return somma / sommaPesi;
+        }
+
+        /// <summary>
+        /// Azzera i valori inseriti per la statistica
+        /// </summary>
+        public void ClearValue()
+        {
+            n_Numeri = 0;
+            somma = 0;
+        }
+
+        /// <summary>
+        /// Ritorna il numero di elementi inseriti per la statistica
+        /// </summary>
+        /// <returns></returns>
+        public int CountValue()
+        {
+            return n_Numeri;
+        }
+
+    }
+}
